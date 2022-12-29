7f6054d07c9cb5a9424927b8ac754b9a85c10be7
Juno <doplitog@gmail.com>
2022-12-29 15:27:19 -0800

    Add delete button, chain inputs, keyboard input
    Adjusted several functions to allow user to string together
    several operations. Added delete button to allow for backspace
    function, and added they ability to use calculator via keyboard.
    
    Changed button styling to grid to fit the calculator better.
    
    Changes to be committed:
    	modified:   index.html
    	modified:   main.js
    	modified:   styles.css

--- main.js
+++ main.js
@@ -1,92 +1,177 @@
+let currentNum = '';
+let previousNum = '';
 let operator = '';
-let previousValue = '';
-let currentValue = '';
 
 document.addEventListener("DOMContentLoaded", function(){
-    // Store all components on HTML into main.js
-    let clear = document.querySelector('#clear-btn');
-    let equal = document.querySelector('.equal');
-    let decimal = document.querySelector('.decimal');
 
-    let numbers = document.querySelectorAll('.number');
-    let operators = document.querySelectorAll('.operator');
+    window.addEventListener('keydown', handleKeys);
 
-    let previousNum = document.querySelector('.previous');
-    let currentNum = document.querySelector('.current');
+    const currentDisplayNum = document.querySelector('.current');
+    const previousDisplayNum = document.querySelector('.previous');
 
-    numbers.forEach((number) => number.addEventListener('click', function(e){
-        getNumber(e.target.textContent)
-        currentNum.textContent = currentValue;
-    }));
+    const equal = document.querySelector('.equal');
 
-    operators.forEach((op) => op.addEventListener('click', function(e){
-        getOperator(e.target.textContent)
-        previousNum.textContent = previousValue + ' ' + operator;
-        currentNum.textContent = currentValue;
-    }))
+    equal.addEventListener('click', () => {
+        if (currentNum != '' && previousNum != '') {
+            calculate();
+        }
+    });
 
-    clear.addEventListener('click', function(){
-        previousValue = '';
-        currentValue = '';
-        operator = '';
-        previousNum.textContent = currentValue;
-        currentNum.textContent = currentValue;
-    })
+    const decimal = document.querySelector('.decimal');
 
-    equal.addEventListener('click', function(){
-        if(currentValue != '' && previousValue != ''){
+    decimal.addEventListener('click', () => {
+        addDecimal();
+    });
+
+    const clear = document.querySelector('.clear');
+
+    clear.addEventListener('click', clearDisplay);
+
+    const deleteButton = document.querySelector('.delete');
+
+    deleteButton.addEventListener('click', handleDelete);
+
+    const numberButtons = document.querySelectorAll('.number');
+
+    numberButtons.forEach((btn) => {
+        btn.addEventListener('click', (e) => {
+            handleNumber(e.target.textContent);
+        });
+    });
+
+    const operators = document.querySelectorAll('.operator');
+
+    operators.forEach((btn) => {
+        btn.addEventListener('click', (e) => {
+            handleOperator(e.target.textContent);
+        });
+    });
+
+    function handleNumber(number) {
+        if(previousNum !== '' && currentNum != '' && operator === '') {
+            previousNum = ''
+            currentDisplayNum.textContent = currentNum
+        }
+        if(currentNum.length <= 10) {
+            currentNum += number;
+            currentDisplayNum.textContent = currentNum;
+        }
+    }
+
+    function handleOperator(op) {
+        if(previousNum === '') {
+            previousNum = currentNum;
+            operatorCheck(op)
+        } else if (currentNum === '') {
+            operatorCheck(op)
+        } else {
             calculate()
-            previousNum.textContent = '';
-            if(previousValue.length <= 5){
-                currentNum.textContent = previousValue;
-            } else {
-                currentNum.textContent = previousValue.slice(0,5) + '...';
+            operator = op;
+            currentDisplayNum.textContent = '';
+            previousDisplayNum.textContent = previousNum + ' ' + operator;
+        }
+    }
+
+    function operatorCheck(text) {
+        operator = text;
+        previousDisplayNum.textContent = previousNum + ' ' + operator;
+        currentDisplayNum.textContent = '';
+        currentNum = '';
+    }
+
+    function calculate() {
+        previousNum = Number(previousNum);
+        currentNum = Number(currentNum);
+    
+        if (operator === '+') {
+            previousNum += currentNum;
+        } else if (operator === '-') {
+            previousNum -= currentNum;
+        } else if (operator === 'x') {
+            previousNum *= currentNum;
+        } else if (operator === '/') {
+            if(currentNum <= 0) {
+               previousNum = 'Error, can\'t divide by 0';
+               previousDisplayNum.textContent = '';
+               currentDisplayNum.textContent = previousNum;
+               operator = '';
+               return; 
+            }
+            previousNum /= currentNum;
+        }
+        previousNum = roundNumber(previousNum);
+        previousNum = previousNum.toString();
+        displayResult();
+    }
+
+    function roundNumber(num) {
+        return Math.round(num * 1000) / 1000;
+    }
+
+    function displayResult() {
+        if (previousNum.length <= 10) {
+            currentDisplayNum.textContent = previousNum;
+        } else {
+            currentDisplayNum.textContent = previousNum.slice(0, 10) + '...';
+        }
+        previousDisplayNum.textContent = '';
+        operator = '';
+        currentNum = '';
+    }
+
+    function clearDisplay() {
+        currentNum = '';
+        previousNum = '';
+        operator = '';
+        currentDisplayNum.textContent = '';
+        previousDisplayNum.textContent = '';
+    }
+
+    function addDecimal() {
+        if (!currentNum.includes('.')) {
+            currentNum += '.'
+            currentDisplayNum.textContent = currentNum;
+        }
+    }
+
+    function handleKeys(e) {
+        e.preventDefault()
+        if (e.key >= 0 && e.key <= 9) {
+            handleNumber(e.key)
+        }
+        if (e.key === 'Enter' || (e.key === '=' && currentNum != '' && previousNum != '')) {
+            calculate();
+        }
+        if (e.key === '+' || e.key === '-' || e.key === '/') {
+            handleOperator(e.key);
+        }
+        if (e.key === '*') {
+            handleOperator('x');
+        }
+        if (e.key === '.') {
+            addDecimal();
+        }
+        if (e.key === 'Backspace') {
+            handleDelete();
+        }
+        if (e.key === 'Delete' || e.key === 'Escape')
+            clearDisplay();
+
+    }
+
+    function handleDelete() {
+        if(currentNum != '') {
+            currentNum = currentNum.slice(0, -1);
+            currentDisplayNum.textContent = currentNum;
+            if (currentNum === '') {
+                currentDisplayNum.textContent = '';
             }
         }
-    })
+        if (currentNum === '' && previousNum !== '' && operator === '') {
+            previousNum = previousNum.slice(0, -1);
+            currentDisplayNum.textContent = previousNum;
+        }
+    }
 
-    decimal.addEventListener('click', function(){
-        addDecimal();
-    })
+
 });
-
-function getNumber(num){
-    if(currentValue.length <=5){
-    currentValue += num;
-    }
-}
-
-function getOperator(op){
-    operator = op;
-    previousValue = currentValue;
-    currentValue = '';
-}
-
-function calculate(){
-    previousValue = Number(previousValue)
-    currentValue = Number(currentValue);
-
-    if(operator === '+'){
-        previousValue += currentValue;
-    } else if (operator === '-'){
-        previousValue -+ currentValue;
-    } else if (operator === 'x'){
-        previousValue *= currentValue;
-    } else {
-        previousValue /= currentValue;
-    }
-
-    previousValue = roundNumber(previousValue);
-    previousValue = previousValue.toString();
-    currentValue = currentValue.toString();
-}
-
-function roundNumber(num){
-    return Math.round(num * 1000) / 1000;
-}
-
-function addDecimal(){
-    if(!currentValue.includes('.')){
-        currentValue += '.';
-    }
-}
